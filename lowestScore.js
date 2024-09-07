80eb10c91614b0c3c3f8ea75939c34ba3880a4e5
Sergey Gasparyan <gasparyan.sego.07@gmail.com>
2024-09-07 21:57:15 +0400

    I write lowestScore and maxPayment

---
+++ lowestScore.js
@@ -0,0 +1,82 @@
+const evaluations = [
+    {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    score: 70
+    },
+    {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 60
+    },
+    {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 3, // Unique identifier for each course
+    courseName: "JS Level 1",
+    score: 50
+    },
+    {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    
+    score: 60
+    },
+    {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 50
+    },
+    {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 3, // Unique identifier for each course
+    courseName: "JS Level 1",
+    score: 40
+    },
+    {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    score: 45
+    },
+    {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 35
+    },
+    {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 3, // Unique identifier for each course
+    courseName: "JS Level 1",
+    score: 25
+    }
+    ];
+
+
+
+function getLowest(arr = []) {
+    let result = +Infinity
+    let res = {}
+    arr.forEach((it) => {
+        if(it.score < result) {
+            res = {...it}
+            result = it.score
+        }
+    })
+        
+    return res
+} 
+console.log(getLowest(evaluations))
